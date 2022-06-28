377e88dfcf896e6cf87b1708b9b1d86457fc8ecb
BackG <37922608+jw6133@users.noreply.github.com>
2022-06-28 18:03:39 +0900

    Add files via upload

---
+++ fuctionobj.js
@@ -0,0 +1,9 @@
+const player = {
+    name : "back",
+    sayHello : function (othername){
+        console.log("hello! "+othername);
+    },
+};
+
+console.log(player.name);
+player.sayHello("nico");

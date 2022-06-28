df8a95de16a6742632e25ae4edd8538c1ea90ff2
BackG <37922608+jw6133@users.noreply.github.com>
2022-06-28 16:06:23 +0900

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,13 @@
+const a = 10;
+const b = 2;
+var c = "metamong"
+let myName ="Back"; 
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello "+myName);
+
+myName = "Jiwoong";
+
+console.log("your new name is"+myName);
